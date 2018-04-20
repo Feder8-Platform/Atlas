c45d9cc5146ea471a08221f96e565b4f96486450
Sander Bylemans <sbylemans@gmail.com>
2018-04-20 11:21:36 +0200

    config-local adjusted for Amazon

--- js/config-local.js
+++ js/config-local.js
@@ -2,7 +2,7 @@ define([], function () {
     var config = {};
     config.api = {
         name: 'Continuum - Janssen Pharmaceutica',
-        url: 'http://cas-dev.eu-west-1.elasticbeanstalk.com/webapi/'
+        url: 'http://cas-dev.eu-west-1.elasticbeanstalk.com/WebAPI/'
     };
     config.userAuthenticationEnabled = true;
     config.honeurEnabled = true;
