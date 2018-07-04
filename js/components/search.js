beec6359046489ab9ce5e17cf2a7b8a58d085cf4
Sander Bylemans <sbylemans@gmail.com>
2018-07-04 09:35:46 +0200

    Disable # of subjects for vocabulary search

--- js/components/search.js
+++ js/components/search.js
@@ -115,11 +115,12 @@ define(['jquery', 'knockout', 'text!./search.html', 'vocabularyprovider', 'atlas
 				'binding': function (o) {
 					return parseInt(o.DESCENDANT_RECORD_COUNT.toString().replace(',', '')) > 0;
 				}
-            }, {
-                'caption': 'Has subjects',
-                'binding': function (o) {
-                    return parseInt(o.PERSON_COUNT.toString().replace(',', '')) > 0;
-                }
+				//TODO: implement this functionality
+            // }, {
+            //     'caption': 'Has subjects',
+            //     'binding': function (o) {
+            //         return parseInt(o.PERSON_COUNT.toString().replace(',', '')) > 0;
+            //     }
             }]
 		};
 
