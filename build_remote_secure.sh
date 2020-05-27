e99bbac6aef39629917f026627ddefb3ece45b32
Sus Verwimp <sus.verwimp@hotmail.com>
2020-05-27 14:51:12 +0200

    build remote BUG

--- build_remote_secure.sh
+++ build_remote_secure.sh
@@ -2,9 +2,9 @@ echo "define([], function () {">js/config-local.js
 echo "   var config = {};">>js/config-local.js
 echo "   config.api = {">>js/config-local.js
 echo "       name: 'HONEUR',">>js/config-local.js
-echo "       url: 'http://localhost:8085/webapi/'">>js/config-local.js
+echo "       url: 'http://localhost:8080/webapi/'">>js/config-local.js
 echo "   };">>js/config-local.js
-echo "   config.userAuthenticationEnabled = false;">>js/config-local.js
+echo "   config.userAuthenticationEnabled = true;">>js/config-local.js
 echo "   config.honeurEnabled = true;">>js/config-local.js
 echo "   config.isCentralInstance = false;">>js/config-local.js
 echo "   config.authProviders = [{">>js/config-local.js
