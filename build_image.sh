eb640e4ced76f894763525b05ddb22331a0b0a4d
Sus Verwimp <sus.verwimp@hotmail.com>
2021-04-16 09:19:36 +0200

    Revert "build to atlas registry copy paste error"
    This reverts commit 7d316e9d6fd1d638828cb3d2a86013574af732a1.

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-/kaniko/executor -f $(pwd)/Dockerfile -c $(pwd) --cache=true --destination=973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/atlas:latest
+/kaniko/executor -f $(pwd)/Dockerfile -c $(pwd) --cache=true --destination=973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/storage:latest
