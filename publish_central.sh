e73cf34dc75b68a367c3153ea1d476d9ceb07f90
Sus Verwimp <sus.verwimp@hotmail.com>
2022-02-24 14:02:27 +0100

    update for develop branch

--- publish_central.sh
+++ publish_central.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-docker tag feder8/atlas:latest 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/atlas:1.10-UAT
+docker tag feder8/atlas:latest 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/atlas:latest
 export AWS_DEFAULT_REGION='eu-west-1'
 aws ecr get-login --no-include-email | bash
-docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/atlas:1.10-UAT
+docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/atlas:latest
