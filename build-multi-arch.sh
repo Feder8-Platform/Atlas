d8e38a1a70af06d83cf6ffc0817df905219e7a6d
Peter Moorthamer <moorthamer@gmail.com>
2022-09-18 11:52:30 +0200

    Feder8 local installation: make it work on Mac with Apple M1 chip (JIRA AEKR-1506)

--- build-multi-arch.sh
+++ build-multi-arch.sh
@@ -3,9 +3,23 @@ set -eux
 
 VERSION=2.0.1
 TAG=2.9.0-$VERSION
-export REGISTRY=harbor-uat.honeur.org
-export REPOSITORY=honeur
-export REGISTRY_USERNAME=admin
-export REGISTRY_PASSWORD=harbor_password
 
+REGISTRY=harbor.honeur.org
+REPOSITORY=honeur
+docker buildx build --platform linux/amd64,linux/arm64 --rm --pull --push  -f "Dockerfile" -t $REGISTRY/$REPOSITORY/atlas:$TAG "."
+
+REGISTRY=harbor.athenafederation.org
+REPOSITORY=athena
+docker buildx build --platform linux/amd64,linux/arm64 --rm --pull --push  -f "Dockerfile" -t $REGISTRY/$REPOSITORY/atlas:$TAG "."
+
+REGISTRY=harbor.lupusnet.org
+REPOSITORY=lupus
+docker buildx build --platform linux/amd64,linux/arm64 --rm --pull --push  -f "Dockerfile" -t $REGISTRY/$REPOSITORY/atlas:$TAG "."
+
+REGISTRY=harbor.esfurn.org
+REPOSITORY=esfurn
+docker buildx build --platform linux/amd64,linux/arm64 --rm --pull --push  -f "Dockerfile" -t $REGISTRY/$REPOSITORY/atlas:$TAG "."
+
+REGISTRY=harbor.phederation.org
+REPOSITORY=phederation
 docker buildx build --platform linux/amd64,linux/arm64 --rm --pull --push  -f "Dockerfile" -t $REGISTRY/$REPOSITORY/atlas:$TAG "."
